cb0ed21c56a49b7bd68424ab4fecd008b10d6b3f
Philippe Beck <philippebeck.dev@gmail.com>
2024-10-10 20:14:14 +0200

    Create Quarantine class with methods

--- hospital-lib/src/quarantine.ts
+++ hospital-lib/src/quarantine.ts
@@ -1,22 +1,101 @@
-import {PatientsRegister} from './patientsRegister';
+import { PatientsRegister } from './patientsRegister';
 
+/**
+ * @class
+ * @name Quarantine
+ * @description - Simulate disease on a group of patients
+ */
 export class Quarantine {
 
-    private static readonly NOT_IMPLEMENTED_MESSAGE = 'Work, work.';
+  /**
+   * @property {PatientsRegister} patients
+   * @description - The patient states register
+   */
+  private patients: PatientsRegister;
 
-    constructor(patients: PatientsRegister) {
-        throw new Error(Quarantine.NOT_IMPLEMENTED_MESSAGE);
+  /**
+   * @property {string[]} drugs
+   * @description - The drugs that should be given to the patients
+   */
+  private drugs: string[];
+
+  /**
+   * @constructor
+   * @description - Create a new Quarantine with the given patients register
+   * @param {PatientsRegister} patients - The patient states register
+   *  The patient states are as follows:
+   *  - F : fever
+   *  - H : healthy
+   *  - D : diabetic
+   *  - T : tuberculosis
+   *  - X : dead
+   */
+  constructor(patients: PatientsRegister) {
+    this.patients = patients;
+    this.drugs    = [];
+  }
+
+  /**
+   * @method setDrugs
+   * @description - Set the drugs that should be given to all the patients
+   * @param {string[]} drugs - The drugs to give to the patients
+   *  The drugs are as follows:
+   *  - An : Antibiotic
+   *  - As : Aspirin
+   *  - I  : Insulin
+   *  - P  : Paracetamol
+   */
+  public setDrugs(drugs: string[]): void {
+    this.drugs = drugs;
+  }
+
+  /**
+   * @method wait40Days
+   * @description - Simulates what happens in the quarantine after 40 days
+   *  The rules are as follows:
+   *  - If a patient has aspirin & paracetamol, he will die
+   *  - If a healthy patient has antibiotics & insulin, the patient will have a fever
+   *  - If a feverish patient has aspirin or paracetamol, the fever will be cured
+   *  - If a tuberculosis patient has antibiotics, the tuberculosis will be cured
+   *  - If a diabetic patient does not have insulin, he will die
+   *  Then the results are stored in the quarantine patients register
+   */
+  public wait40Days(): void {
+    const HAS_ANTIBIOTIC: boolean  = this.drugs.includes('An');
+    const HAS_ASPIRIN: boolean     = this.drugs.includes('As');
+    const HAS_INSULIN: boolean     = this.drugs.includes('I');
+    const HAS_PARACETAMOL: boolean = this.drugs.includes('P');
+
+    let {F, H, D, T}: PatientsRegister = this.patients;
+    let newPatients: PatientsRegister  = { F: 0, H: 0, D: 0, T: 0, X: 0 };
+
+    if (HAS_ASPIRIN && HAS_PARACETAMOL) {
+      newPatients.X += F + H + D + T;
+
+    } else {
+
+      if (HAS_ANTIBIOTIC && HAS_INSULIN) newPatients.F += H;
+      else newPatients.H += H;
+
+      if (HAS_ASPIRIN || HAS_PARACETAMOL) newPatients.H += F;
+      else newPatients.F += F;
+
+      if (HAS_ANTIBIOTIC) newPatients.H += T;
+      else newPatients.T += T;
+
+      if (!HAS_INSULIN) newPatients.X += D;
+      else newPatients.D += D;
     }
 
-    public setDrugs(drugs: Array<string>): void {
-        throw new Error(Quarantine.NOT_IMPLEMENTED_MESSAGE);
-    }
+    this.patients = newPatients;
+  }
 
-    public wait40Days(): void {
-        throw new Error(Quarantine.NOT_IMPLEMENTED_MESSAGE);
-    }
-
-    public report(): PatientsRegister {
-        throw new Error(Quarantine.NOT_IMPLEMENTED_MESSAGE);
-    }
+  /**
+   * @method report
+   * @description - Return the current state of the patients
+   * @returns {PatientsRegister} - The current state of the patients
+   */
+  public report(): PatientsRegister {
+    return this.patients;
+  }
 }
