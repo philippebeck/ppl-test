7a62ca42cf3100b0f978e33e2a44957267169544
Philippe Beck <philippebeck.dev@gmail.com>
2024-10-18 14:11:31 +0200

    Add results part to App

--- hospital-fe/src/App.vue
+++ hospital-fe/src/App.vue
@@ -2,17 +2,22 @@
 import { ref } from 'vue'
 import axios from 'axios'
 import { PatientsRegister } from 'hospital-lib'
+// TODO: import the Quarantine class from the dist folder
+import { Quarantine } from 'hospital-lib/src/quarantine'
 
 import Title from './components/atoms/Title.vue'
 import Button from './components/atoms/Button.vue'
 import Patients from './components/molecules/Patients.vue'
 import Drugs from './components/molecules/Drugs.vue'
+import Results from './components/molecules/Results.vue'
 
 const patientsLoaded = ref<boolean>(false)
 const drugsLoaded    = ref<boolean>(false)
+const resultsLoaded  = ref<boolean>(false)
 
 const patients = ref<PatientsRegister | undefined>({})
 const drugs    = ref<string[] | undefined>([])
+const results  = ref<PatientsRegister | undefined>({})
 
 const getData = async (url: string) => {
   try {
@@ -51,14 +56,39 @@ const loadData = async () => {
   await loadPatients();
   await loadDrugs();
 }
+
+const reportResults = async () => {
+  if (patients.value) {
+    const quarantine = new Quarantine(patients.value);
+
+    quarantine.setDrugs(drugs.value);
+    quarantine.wait40Days();
+    
+    results.value = quarantine.report();
+    resultsLoaded.value = true;
+
+  } else {
+    console.error('Patients data is undefined. Cannot create Quarantine.');
+  }
+}
 </script>
 
 <template>
   <Title title="Hospital" sub="The Quarantine Simulation" :lvl="1"></Title>
 
   <Button :action="loadData" label="Load the Patients & the Drugs" />
+
   <Patients v-if="patientsLoaded" :patients="patients" />
   <Drugs v-if="drugsLoaded" :drugs="drugs" />
+
+  <Button
+    v-if="patientsLoaded && drugsLoaded"
+    :action="reportResults"
+    label="Dispense the Drugs"
+  />
+  
+  <!-- TODO: link the patients & the results-->
+  <Results v-if="resultsLoaded" :patients="patients" :results="results" />
 </template>
 
 <style scoped></style>
