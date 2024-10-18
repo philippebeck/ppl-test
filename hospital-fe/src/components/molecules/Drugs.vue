368719fd6c36ebcf2bd775938141f7823f2e7222
Philippe Beck <philippebeck.dev@gmail.com>
2024-10-18 11:58:33 +0200

    Create Drugs molecule

---
+++ hospital-fe/src/components/molecules/Drugs.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import Title from '../atoms/Title.vue'
+
+defineProps<{ drugs: string[] | undefined }>()
+
+</script>
+
+<template>
+  <section>
+    <Title title="Drugs" sub="The List of Drugs" />
+
+    <ul>
+      <li v-for="drug in drugs" :key="drug">{{ drug }}</li>
+    </ul>
+  </section>
+</template>
+
+<style scoped>
+  section {
+    display: inline-block;
+    margin: 20px;
+    border-radius: 20px;
+    padding: 20px;
+    background-color: #FFF;
+    color: #000;
+  }
+
+  ul {
+    display: flex;
+    place-content: space-evenly;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+</style>
