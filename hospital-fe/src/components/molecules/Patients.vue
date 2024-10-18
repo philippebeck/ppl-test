1f0c434f9ac0240144720ef09ac9e97215e6810a
Philippe Beck <philippebeck.dev@gmail.com>
2024-10-18 11:58:24 +0200

    Create Patients molecule

---
+++ hospital-fe/src/components/molecules/Patients.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { PatientsRegister } from 'hospital-lib';
+import Title from '../atoms/Title.vue'
+
+defineProps<{ patients: PatientsRegister | undefined }>()
+
+</script>
+
+<template>
+  <section>
+    <Title title="Patients" sub="The List of Patients" />
+
+    <ul>
+      <li v-for="(value, key) in patients" :key="key">{{ key }} : {{ value }}</li>
+    </ul>
+  </section>
+</template>
+
+<style scoped>
+  section {
+    display: inline-block;
+    margin: 20px;
+    border-radius: 20px;
+    padding: 20px;
+    background-color: #FFF;
+    color: #000;
+  }
+
+  ul {
+    display: flex;
+    place-content: space-evenly;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+</style>
