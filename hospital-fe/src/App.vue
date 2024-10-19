6b8ae4f3d14c9f2e3c14fef123466ea0d83d1861
Philippe Beck <philippebeck.dev@gmail.com>
2024-10-19 12:53:43 +0200

    App indentation

--- hospital-fe/src/App.vue
+++ hospital-fe/src/App.vue
@@ -1,93 +1,93 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import axios from 'axios'
-import { PatientsRegister } from 'hospital-lib'
-// TODO: import the Quarantine class from the dist folder
-import { Quarantine } from 'hospital-lib/src/quarantine'
+  import { ref } from 'vue'
+  import axios from 'axios'
+  import { PatientsRegister } from 'hospital-lib'
+  // TODO: import the Quarantine class from the dist folder
+  import { Quarantine } from 'hospital-lib/src/quarantine'
 
-import Title from './components/atoms/Title.vue'
-import Button from './components/atoms/Button.vue'
-import Patients from './components/molecules/Patients.vue'
-import Drugs from './components/molecules/Drugs.vue'
-import Results from './components/molecules/Results.vue'
+  import Title from './components/atoms/Title.vue'
+  import Button from './components/atoms/Button.vue'
+  import Patients from './components/molecules/Patients.vue'
+  import Drugs from './components/molecules/Drugs.vue'
+  import Results from './components/molecules/Results.vue'
 
-const totalTests     = ref<number>(0)
-const patientsLoaded = ref<boolean>(false)
-const drugsLoaded    = ref<boolean>(false)
-const resultsLoaded  = ref<boolean>(false)
+  const totalTests     = ref<number>(0)
+  const patientsLoaded = ref<boolean>(false)
+  const drugsLoaded    = ref<boolean>(false)
+  const resultsLoaded  = ref<boolean>(false)
 
-const patients     = ref<PatientsRegister | undefined>({})
-const drugs        = ref<string[] | undefined>([])
-const currentDrugs = ref<string[] | undefined>([])
-const drugsList    = ref<string[]>([])
-const results      = ref<{ [key: string]: { input: number, output: number } }>({})
-const resultsList  = ref<{ input: number, output: number }[]>([])
+  const patients     = ref<PatientsRegister | undefined>({})
+  const drugs        = ref<string[] | undefined>([])
+  const currentDrugs = ref<string[] | undefined>([])
+  const drugsList    = ref<string[]>([])
+  const results      = ref<{ [key: string]: { input: number, output: number } }>({})
+  const resultsList  = ref<{ input: number, output: number }[]>([])
 
-/**
- * @method getData
- *
- * @description
- *  Make a GET request to the given endpoint & return the response data
- *
- * @param {string} endpoint
- *  The endpoint to make the request to
- *
- * @returns {Promise<string | undefined>}
- *  The response data of the request
- */
-const getData = async (endpoint: string) => {
-  const URL = `http://localhost:7200/${endpoint}`
+  /**
+   * @method getData
+   *
+   * @description
+   *  Make a GET request to the given endpoint & return the response data
+   *
+   * @param {string} endpoint
+   *  The endpoint to make the request to
+   *
+   * @returns {Promise<string | undefined>}
+   *  The response data of the request
+   */
+  const getData = async (endpoint: string) => {
+    const URL = `http://localhost:7200/${endpoint}`
 
-  try {
-    const response = await axios.get<string>(URL)
+    try {
+      const response = await axios.get<string>(URL)
 
-    return response.data
+      return response.data
 
-  } catch (error) {
-    console.error(error)
+    } catch (error) {
+      console.error(error)
+    }
   }
-}
 
-/**
- * @method formatPatientsData
- *
- * @description
- *  Parses a string of patient states & counts occurrences of each state
- *
- * @param {string} data
- *  A comma-separated string representing patient states
- *
- * @returns {Object}
- *  An object with the counts of each state, initialized with default values
- */
-const formatPatientsData = (data) => {
+  /**
+   * @method formatPatientsData
+   *
+   * @description
+   *  Parses a string of patient states & counts occurrences of each state
+   *
+   * @param {string} data
+   *  A comma-separated string representing patient states
+   *
+   * @returns {Object}
+   *  An object with the counts of each state, initialized with default values
+   */
+  const formatPatientsData = (data) => {
 
-  return data
-    ?.split(',')
-    .reduce((acc: { [key: string]: number }, current: string) => {
-      const defaultState = { F: 0, H: 0, D: 0, T: 0, X: 0 }
+    return data
+      ?.split(',')
+      .reduce((acc: { [key: string]: number }, current: string) => {
+        const defaultState = { F: 0, H: 0, D: 0, T: 0, X: 0 }
 
-      acc = { ...defaultState, ...acc }
-      acc[current] = (acc[current] || 0) + 1
+        acc = { ...defaultState, ...acc }
+        acc[current] = (acc[current] || 0) + 1
 
-      return acc
-    }, {})
-}
+        return acc
+      }, {})
+  }
 
-/**
- * @method loadPatients
- *
- * @description
- *  Load the patients from the API & put them in the state
- *
- * @returns {Promise<void>}
- */
-const loadPatients = async () => {
-  const data = await getData("patients")
+  /**
+   * @method loadPatients
+   *
+   * @description
+   *  Load the patients from the API & put them in the state
+   *
+   * @returns {Promise<void>}
+   */
+  const loadPatients = async () => {
+    const data = await getData("patients")
 
-  patients.value       = formatPatientsData(data)
-  patientsLoaded.value = true
-}
+    patients.value       = formatPatientsData(data)
+    patientsLoaded.value = true
+  }
 
   /**
    * @method loadDrugs
@@ -97,14 +97,14 @@ const loadPatients = async () => {
    *
    * @returns {Promise<void>}
    */
-const loadDrugs = async () => {
-  const data = await getData('drugs')
+  const loadDrugs = async () => {
+    const data = await getData('drugs')
 
-  currentDrugs.value = data
-  drugs.value = data?.split(',')
+    currentDrugs.value = data
+    drugs.value = data?.split(',')
 
-  drugsLoaded.value = true
-}
+    drugsLoaded.value = true
+  }
 
   /**
    * @method loadData
@@ -114,10 +114,10 @@ const loadDrugs = async () => {
    *
    * @returns {Promise<void>}
    */
-const loadData = async () => {
-  await loadPatients()
-  await loadDrugs()
-}
+  const loadData = async () => {
+    await loadPatients()
+    await loadDrugs()
+  }
 
   /**
    * @method reportResults
@@ -127,42 +127,42 @@ const loadData = async () => {
    *
    * @returns {Promise<void>}
    */
-const reportResults = async () => {
-  if (patients.value) {
-    const quarantine   = new Quarantine(patients.value)
+  const reportResults = async () => {
+    if (patients.value) {
+      const quarantine   = new Quarantine(patients.value)
 
-    quarantine.setDrugs(currentDrugs.value)
-    quarantine.wait40Days()
+      quarantine.setDrugs(currentDrugs.value)
+      quarantine.wait40Days()
 
-    const newResult = Object
-      .keys(patients.value)
-      .reduce((acc, key) => {
+      const newResult = Object
+        .keys(patients.value)
+        .reduce((acc, key) => {
 
-        acc[key] = {
-          input: patients.value[key],
-          output: quarantine.report()[key]
-        }
+          acc[key] = {
+            input: patients.value[key],
+            output: quarantine.report()[key]
+          }
 
-        return acc
-      }, {})
+          return acc
+        }, {})
 
-    resultsList.value.push(newResult)
-    drugsList.value.push(currentDrugs.value.slice())
+      resultsList.value.push(newResult)
+      drugsList.value.push(currentDrugs.value.slice())
 
-    if (resultsList.value.length > 10) {
-      resultsList.value.shift()
-      drugsList.value.shift()
+      if (resultsList.value.length > 10) {
+        resultsList.value.shift()
+        drugsList.value.shift()
+      }
+
+      totalTests.value++
+      resultsLoaded.value = true
+
+    } else {
+      console.error(
+        'Patients data is undefined. Cannot create Quarantine.'
+      )
     }
-
-    totalTests.value++
-    resultsLoaded.value = true
-
-  } else {
-    console.error(
-      'Patients data is undefined. Cannot create Quarantine.'
-    )
   }
-}
 </script>
 
 <template>
