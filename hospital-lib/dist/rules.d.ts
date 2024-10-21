c9ee3b908a6eeb707ff0c8de35098ddf5a1f3715
Philippe Beck <philippebeck.dev@gmail.com>
2024-10-21 22:59:15 +0200

    Update dist

--- hospital-lib/dist/rules.d.ts
+++ hospital-lib/dist/rules.d.ts
@@ -11,6 +11,8 @@ import { PatientsRegister as PR } from './patientsRegister';
  * @description
  *  Dead rules
  *  - If the patients have Aspirin & Paracetamol, they will die
+ * * (additional test to check the logic flexibility)
+ * * - If the patients have Aspirin & Ibuprofen, they will die
  */
 export declare const deadRules: Array<{
     condition: (drugs: string[]) => boolean;
@@ -19,7 +21,6 @@ export declare const deadRules: Array<{
 }>;
 /**
  * @constant treatmentRules
- *
  * @type {Array<{
  *  condition: (drugs: string[]) => boolean;
  *  valid: (patients: PR, newPatients: PR) => void;
@@ -29,7 +30,9 @@ export declare const deadRules: Array<{
  * @description
  *  Treatment rules
  *  - If healthy patients have Antibiotic & Insulin, they will have a fever
- *  - If feverish patients have Aspirin or Paracetamol, the fever will be cured
+ * * (modified test to check the logic flexibility)
+ * * - If feverish patients have Aspirin or Ibuprofen or Paracetamol, the fever will be cured
+ * * - If painful patients have Aspirin or Ibuprofen or Paracetamol, the pain will be cured
  *  - If tuberculosis patients have Antibiotic, the tuberculosis will be cured
  *  - If diabetic patients have not Insulin, they will die
  */
